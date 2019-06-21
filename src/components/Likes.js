79f84aa419c1cd612c36169cc2e0a184b44f92b0
madeInAba <ezeokecc@gmail.com>
2019-06-21 13:59:42 +0100

    state introductory class correction

--- src/components/Likes.js
+++ src/components/Likes.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 class Likes extends React.Component{
  state = {
-  likes: this.state.likes
- }
+  likes: 10}
+ 
 
  render(){
   return(
    <div>
     <h1>Welcome to my likes app</h1>
-    <h3>likes: 0</h3>
+    <h3>likes: {this.state.likes}</h3>
     <button>like</button>
     <button>unlike</button>
     <button>reset</button>
