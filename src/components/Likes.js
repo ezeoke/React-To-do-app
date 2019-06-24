1c04148777d4c2f7896a0b6ee31af3fc22042847
madeInAba <ezeokecc@gmail.com>
2019-06-24 13:09:54 +0100

    tweaking setstate

--- src/components/Likes.js
+++ src/components/Likes.js
@@ -1,17 +1,34 @@
+
 import React from 'react';
 
 class Likes extends React.Component{
  state = {
-  likes: 10}
+  likes: 0,
+   grace: true}
  
+ increaseLikes = () => {
+ this.setState((prevState)=>{
+   return {
+    likes: prevState.likes + 1,
+    grace: prevState.grace = false
+   }
+ })
+}
+
+decreaseLikes = () => { this.setState((prevState)=>{
+ return {
+  likes: prevState.likes - 1
+ }
+})
+}
 
  render(){
   return(
    <div>
     <h1>Welcome to my likes app</h1>
     <h3>likes: {this.state.likes}</h3>
-    <button>like</button>
-    <button>unlike</button>
+    <button onClick ={this.increaseLikes}>like</button>
+    <button onClick ={this.decreaseLikes}>unlike</button>
     <button>reset</button>
    </div>
   )
