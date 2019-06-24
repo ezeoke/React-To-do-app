a3e4c249f9fc1f50e132ef7ee5d73aa88d8c9e8f
madeInAba <ezeokecc@gmail.com>
2019-06-24 14:45:11 +0100

    using inputtags in react

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,23 +1,43 @@
-import React, {Component} from  'react';
+import React, { Component } from 'react';
 import Header from './Header';
 import TodoItems from './TodoItems';
 
 class Todo extends Component {
-state = {
-  todoItems: ['cook', 'eat', 'sleep', 'relax', 'born']
-}
+  state = {
+    todoItems: ['cook', 'eat', 'sleep', 'relax', 'born'],
+    newTodo: ''
+  }
 
-render(){
- 
- return (
-  <div>
-   <Header title='MY TODO TITLE'/>
-  <h1>Welcome to my todo app</h1>
-  <ul>
-   {this.state.todoItems.map((item)=>{ return <TodoItems individualItems = {item}/>})}
-  </ul>
-  </div>
- )
-}}
+  handleChange = (e) => {
+    this.setState({ newTodo: e.target.value })
+    console.log(e.target.value);
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.newTodo)
+  }
+
+  render() {
+
+    return (
+      <div>
+        <Header title='MY TODO TITLE' />
+        <h1>Welcome to my todo app</h1>
+
+        {this.state.todoItems.map((item) => { return <TodoItems individualItems={item} /> })}
+
+        <form onSubmit={this.handleSubmit}>
+          <label>Todo Items</label><br></br>
+          <input type="text" name="todo-item"
+            value={this.state.newTodo}
+            onChange={this.handleChange}
+          ></input><br></br>
+          <button>submit</button>
+        </form>
+      </div>
+    )
+  }
+}
 
 export default Todo;
