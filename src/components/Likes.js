340948625d0fa833ceec78003c409eebab34bad7
cuba <ezeokecc@gmail.com>
2019-07-25 15:47:50 +0100

    edited reset button

--- src/components/Likes.js
+++ src/components/Likes.js
@@ -1,41 +1,45 @@
-import React from 'react';
-import Header from './Header';
+import React from "react";
+import Header from "./Header";
 
 class Likes extends React.Component {
-	state = {
-		likes: 0,
-		grace: true
-	};
+  state = {
+    likes: 0
+  };
 
-	increaseLikes = () => {
-		this.setState((prevState) => {
-			return {
-				likes: prevState.likes + 1,
-				grace: (prevState.grace = false)
-			};
-		});
-	};
+  increaseLikes = () => {
+    this.setState(prevState => {
+      return {
+        likes: prevState.likes + 1
+      };
+    });
+  };
 
-	decreaseLikes = () => {
-		this.setState((prevState) => {
-			return {
-				likes: prevState.likes - 1
-			};
-		});
-	};
+  decreaseLikes = () => {
+    this.setState(prevState => {
+      return {
+        likes: prevState.likes >= 0 && prevState.likes - 1
+      };
+    });
+  };
 
-	render() {
-		return (
-			<div>
-				<Header title="Likes App" />
-				<h1>Welcome to my likes app</h1>
-				<h3>likes: {this.state.likes}</h3>
-				<button onClick={this.increaseLikes}>like</button>
-				<button onClick={this.decreaseLikes}>unlike</button>
-				<button>reset</button>
-			</div>
-		);
-	}
+  resetLikes = () => {
+    this.setState({
+		likes : 0
+	});
+  };
+
+  render() {
+    return (
+      <div>
+        <Header title="Likes App" />
+        <h1>Welcome to my likes app</h1>
+        <h3>likes: {this.state.likes}</h3>
+        <button onClick={this.increaseLikes}>like</button>
+        <button onClick={this.decreaseLikes}>unlike</button>
+        <button onClick={this.resetLikes}>reset</button>
+      </div>
+    );
+  }
 }
 
 export default Likes;
